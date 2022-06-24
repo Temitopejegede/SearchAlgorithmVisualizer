2586d327c76eb4737e963c5664c9d1377233d2c7
Temitope Jegede <temiteejegede@gmail.com>
2022-06-24 17:14:06 -0400

    Initial commit

---
+++ src/main/java/com/visual/searchalgorithmvisualizer/HelloController.java
@@ -0,0 +1,14 @@
+package com.visual.searchalgorithmvisualizer;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+
+public class HelloController {
+    @FXML
+    private Label welcomeText;
+
+    @FXML
+    protected void onHelloButtonClick() {
+        welcomeText.setText("Search algorithm Visualizer");
+    }
+}
